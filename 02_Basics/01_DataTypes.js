4a027d90364618cad3301093d0db7bdf9ae122b7
Kaif Ansari <kaifansari54540@gmail.com>
2024-12-11 14:06:54 +0530

    4th Lecture done

---
+++ 02_Basics/01_DataTypes.js
@@ -0,0 +1,48 @@
+// Primitive DataTypes
+// There are 7 catagories of it
+
+// String
+const name = "Kaif";
+
+// Number
+const num = 7;
+
+// Boolean
+const Bool = true;
+
+// Null
+const Null = null;
+
+// Undefined
+const age = undefined;
+
+// Symbol
+const id = Symbol('123');
+
+// BigInt
+const BigNum = 678768767567567578786n;
+
+// Reference Type also known as Non Primitive
+
+// Array
+const brand = ["apple", "rolex", "samsung", "ronaldo"];
+
+// Objects
+const Data = {
+    name: 'Kaif Ansari',
+    age: 20,
+    occupation: 'Software Developer',
+}
+
+// Functions
+
+const MyFunc = function(){
+    console.log('Heyyy theree....');
+}
+
+
+function helloWorld(){
+    console.log('Hello world');
+}
+
+helloWorld();
