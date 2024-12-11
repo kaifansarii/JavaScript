6c90a1dfac879a24ec672548be0d101248a6acd2
Kaif Ansari <kaifansari54540@gmail.com>
2024-12-11 12:12:31 +0530

    1st Lecture done

--- Basics/01_Basics.js
+++ Basics/01_Basics.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+"Use Strict"; // treat all JS Code as newer version.
+// code readability should be high
+
+// * Variables 
+
+let number = 5;
+let String = "Kaif ansari";
+let BigInt = 565545;
+let boolean = 5 == 5;
+let Null = null;                   // This is an object, It's not Undefined.
+let UnDefined;
+let Symbol = '@';
+
+// Three ways to define a variable
+// let, const, var ('var doesn't work well with the lastest fetures').
+
+// * typeof                       // Used to check the datatype of a variable.
+let name = "Kaif Ansari";
+console.log(typeof name);
