6e65481fed615cae52a154969f955f8c2f4851ca
Kaif Ansari <kaifansari54540@gmail.com>
2024-12-11 12:43:16 +0530

    3rd Lecture done

---
+++ Basics/03_Operations.js
@@ -0,0 +1,61 @@
+
+// ****************** Basic Operations ****************** //
+
+let value = 3;
+let negvalue = -value;
+console.log(value);
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**3);
+console.log(2%3);
+
+
+// ************************************************* //
+
+let name = "Kaif ";
+let surname = "Ansari";
+
+let fullname = name + surname;
+console.log(fullname);
+
+// ************************************************* //
+
+console.log("1" + 2 + 2);
+// output :- 122
+
+console.log(1 + 2 + "2");
+// output :- 32
+
+// ************************************************* //
+
+// Use paranthisis to solve this much vast equations.
+console.log( (5 + 5) * (9 + 9) );
+
+// ************************************************* //
+
+let num1, num2, num3;
+num1 = num2 = num3 = 5 + 5;
+
+// ************************************************* //
+
+// PreFix Operator
+// It will increase the value after the program is executed
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+// output :- 101
+
+// PostFix Operator
+// It will increase the value before the program is executed
+let gameCounterr = 100;
+++gameCounterr;
+console.log(gameCounter);
+// output :- 101
+
+// ************************************************* //
+
+
